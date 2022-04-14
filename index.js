9dffefe1c49e3b0dca9d44e53cd370417d5ddc32
Нуртилек Кошкар <nurtylek98@gmail.com>
2022-04-14 14:00:08 +0600

    add mongoose

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://admin:admin@mongo:27017/?authSource=admin', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error connecting to MongoDB:', error.message);
+});
 
 const port = process.env.PORT ?? 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h2>Hello World from docker-compose.dev.yml</h2>');
+    res.send('<h2>Hello World!!!</h2>');
 })
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}, pid is ${process.pid}`);
+    console.log(`Listening on port: ${port}`);
 });
